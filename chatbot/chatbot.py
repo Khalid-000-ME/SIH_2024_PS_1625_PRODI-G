b5cd50c471b167c13d0c852613eb600940f9f2f6
Khalid M <imtheuser3@gmail.com>
2024-09-08 23:59:02 +0530

    Update chatbot.py
    Wrote the code for basic interaction with Gemini API [Not yet integrated into the site]

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,1 +1,25 @@
+import requests
+import google.generativeai as genai
+import os
 
+API_KEY = 'api_key_here' #API KEY is not mentioned here. Create one in Google AI Studio
+
+genai.configure(api_key=API_KEY)
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+chat = model.start_chat(
+    history=[
+        {"role":"user", "parts":"Hello"},
+        {"role":"model", "parts":"Great to meet you. What would you like to know?"}
+    ]
+)
+response = chat.send_message("What is Quantum Entanglement.", generation_config=genai.types.GenerationConfig(
+    candidate_count=1,
+    stop_sequences=["x"],
+    max_output_tokens=150,
+    temperature=0.5,
+),
+)
+print(response.text)
+response = chat.send_message("Wonderful.")
+print(response.text)
