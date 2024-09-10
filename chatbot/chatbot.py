50e762f4098314048e3880b5e5d688aa3d6f23c4
Khalid M <imtheuser3@gmail.com>
2024-09-10 20:01:43 +0530

    Update chatbot.py
    Updated chatbot.py

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,25 +1,35 @@
-import requests
+from flask import Flask
+from flask_cors import CORS
 import google.generativeai as genai
-import os
 
-API_KEY = 'api_key_here' #API KEY is not mentioned here. Create one in Google AI Studio
+app = Flask(__name__)
+CORS(app)
 
-genai.configure(api_key=API_KEY)
+API_KEY = "AIzaSyBdzYHCOehJj3fLtboBC1Z57_HFv484ico"
 
-model = genai.GenerativeModel("gemini-1.5-flash")
-chat = model.start_chat(
-    history=[
-        {"role":"user", "parts":"Hello"},
-        {"role":"model", "parts":"Great to meet you. What would you like to know?"}
-    ]
-)
-response = chat.send_message("What is Quantum Entanglement.", generation_config=genai.types.GenerationConfig(
-    candidate_count=1,
-    stop_sequences=["x"],
-    max_output_tokens=150,
-    temperature=0.5,
-),
-)
-print(response.text)
-response = chat.send_message("Wonderful.")
-print(response.text)
+@app.route('/greet/<name>')
+
+def greet(name):
+    
+    genai.configure(api_key=API_KEY)
+    
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    chat = model.start_chat(
+        history =[
+            {'role': 'user', 'parts': 'Hello'},
+            {'role': 'model', 'parts': 'Hello, how can I help you with your doubts?'}
+        ]
+    )
+    
+    response = chat.send_message(
+        name,
+        generation_config=genai.types.GenerationConfig(
+            candidate_count=1,
+            temperature=0.5,
+        ),
+    )
+    
+    return response.text
+
+if __name__ == "__main__":
+    app.run(debug=True)
